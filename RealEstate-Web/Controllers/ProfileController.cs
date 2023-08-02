97577a0332eab10f8c61be41b78e134749e90923
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-08-02 22:43:56 +0400

    Add project files.

---
+++ RealEstate-Web/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

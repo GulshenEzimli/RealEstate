5a0bde25c6c096ab11aab24f9f2277683476035e
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-08-06 17:17:25 +0400

    Add CategoryControlller

---
+++ RealEstate-API/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_API.Repositories.UnitOfWork;
+
+namespace RealEstate_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IUnitOfWork _db;
+        public CategoryController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await _db.CategoryRepository.GetAllAsync();
+            return Ok(values);
+        }
+    }
+}

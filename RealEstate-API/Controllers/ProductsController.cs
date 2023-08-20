52ad2bfd85cb0dead6694119b0d727f2d610b2c3
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-08-20 21:10:07 +0400

    Add ProductController

---
+++ RealEstate-API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstateCore.Repositories.UnitOfWork;
+
+namespace RealEstate_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IUnitOfWork _db;
+        public ProductsController(IUnitOfWork db)
+        {
+            _db = db;    
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await _db.ProductRepository.GetAllAsync();
+            return Ok(values);  
+        }
+        [HttpGet("ProductsWithCategories")]
+        public async Task<IActionResult> GetAllProductsWithCategories()
+        {
+            var values = await _db.ProductRepository.GetProductsWithCategories();
+            return Ok(values);
+        }
+    }
+}

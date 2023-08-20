d89a3e3c42a4d93c3b92fc57c0e667328b4fd06f
Gulshan Azimli <Gulshan.Azimli@outlook.com>
2023-08-20 21:09:20 +0400

    Add ProductDto class

---
+++ RealEstateCore/Dtos/ProductDtos/ProductDto.cs
@@ -0,0 +1,23 @@
+﻿using RealEstateCore.Dtos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateCore.Dtos.ProductDtos
+{
+    public class ProductDto :IDto
+    {
+        public int ProductId { get; set; }
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? Address { get; set; }
+        public string? Description { get; set; }
+        public int ProductCategoryId { get; set; }
+        public int EmployeeId { get; set; }
+
+    }
+}
---
+++ RealEstateCore/Dtos/ProductDtos/ProductsWithCategories.cs
@@ -0,0 +1,30 @@
+﻿using RealEstateCore.Dtos.CategoryDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateCore.Dtos.ProductDtos
+{
+    public class ProductsWithCategories
+    {
+        public int ProductId { get; set; }
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? Address { get; set; }
+        public string? Description { get; set; }
+        public int EmployeeId { get; set; }
+        public int CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public ResultCategoryDto Category => new ResultCategoryDto()
+        {
+            CategoryId = CategoryId,
+            CategoryName = CategoryName,
+            CategoryStatus = CategoryStatus,
+        };
+    }
+}
